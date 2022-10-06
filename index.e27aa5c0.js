d29172be8b162989e052a204e2ecd36485a44ac9
Kolesanencore <111977948+Kolesanencore@users.noreply.github.com>
2022-10-06 11:30:25 +0000

    Deploying to gh-pages from @ Kolesanencore/Oh-my-donats@518a5a2c117ad5bdbbef270f196e6c3731297943 🚀

---
+++ index.e27aa5c0.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.body.classList.add("loaded_hiding"),window.setTimeout((function(){document.body.classList.add("loaded"),document.body.classList.remove("loaded_hiding")}),3e3)};
+//# sourceMappingURL=index.e27aa5c0.js.map
